e8fd192c2cf0844f0e16d8aa563edc723add3ab6
irovira-uxdesign <irovira-uxdesign@users.noreply.github.com>
2021-03-16 20:44:41 +0000

    Add Mobile Menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+
 console.log("Hi! Welcome to My Portfolio Site");
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
